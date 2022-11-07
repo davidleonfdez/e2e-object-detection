8091a19891179ae431a300163e41d76764207d15
davidleonfdez <dleoferu@gmail.com>
2022-11-07 18:48:08 +0000

    Add simple Streamlit client

---
+++ client/app.py
@@ -0,0 +1,43 @@
+import io
+from PIL import Image, ImageDraw
+import requests
+import streamlit as st
+
+
+BACKEND_URL = "http://127.0.0.1:8080/predictions/cyclist_detector"
+
+
+def bytes_to_image(bytes_data):
+    return Image.open(io.BytesIO(bytes_data))
+
+
+def draw_detections(detections, img):
+    if len(detections) == 0: 
+        st.text("No objects detected!")
+        return
+
+    draw = ImageDraw.ImageDraw(img)    
+    
+    for detection in detections:
+        draw.rectangle(detection['coords'])
+
+    st.image(img)    
+
+
+def detect_and_draw(uploaded_file):
+    bytes_img = uploaded_file.getvalue()
+    detection_response = requests.post(BACKEND_URL, {'data': bytes_img})
+    if detection_response.ok:
+        img = bytes_to_image(bytes_img)
+        draw_detections(detection_response.json(), img)      
+    else:
+        error_text = f'Error in request to {BACKEND_URL}'
+        st.error(error_text)
+        print(error_text)
+
+
+uploaded_file = st.file_uploader("Upload an image", label_visibility="visible")
+
+
+if uploaded_file is not None:
+    detect_and_draw(uploaded_file)
