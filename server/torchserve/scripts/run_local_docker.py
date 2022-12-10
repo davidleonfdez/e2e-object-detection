d760366ca64110d5783658c40caa3e27e9bd7b6c
davidleonfdez <dleoferu@gmail.com>
2022-12-10 01:09:05 +0000

    Use Docker SDK instead of subprocess to start server locally

--- server/torchserve/scripts/run_local_docker.py
+++ server/torchserve/scripts/run_local_docker.py
@@ -3,6 +3,46 @@ from pathlib import Path
 import subprocess
 
 
+PORTS = [8080, 8081, 8082, 7070, 7071]
+
+
+def _get_base_path():
+    return Path(__file__).resolve().parent.parent
+
+
+def run_with_subprocess(model_store_path):
+    base_path = _get_base_path()
+    ports_binding = ' '.join(f"-p {p}:{p}" for p in PORTS)
+    cmd = (
+        #"docker run --rm -it -p 8080:8080 -p 8081:8081 -p 8082:8082 -p 7070:7070 -p 7071:7071 " +
+        f"docker run --rm -it {ports_binding} " +
+        f"-v {model_store_path}:/home/model-server/model-store " +
+        f"-v {str(base_path/'config.properties')}:/home/model-server/config.properties " +
+        "pytorch/torchserve:latest-cpu torchserve --model-store model-store --start --models all --ncs "
+    )
+    print(cmd)
+    subprocess.run(cmd.split(' '))
+
+
+def run_with_docker_py(model_store_path):
+    import docker
+    client = docker.from_env()
+    base_path = _get_base_path()
+
+    container = client.containers.run(
+        "pytorch/torchserve:latest-cpu",
+        command="torchserve --model-store model-store --start --models all --ncs",
+        volumes=[
+            f"{model_store_path}:/home/model-server/model-store",
+            f"{str(base_path/'config.properties')}:/home/model-server/config.properties",
+        ],
+        ports={p:p for p in PORTS},
+        remove=True,
+        detach=True,
+    )
+    return container
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="""Start TorchServe hosted in a local Docker container"""
@@ -11,14 +51,5 @@ if __name__ == '__main__':
         'model_store_path', type=str, help='Path of the directory that contains the .mar model files'
     )
     args = parser.parse_args()
-
-    base_path = Path(__file__).resolve().parent.parent
-    cmd = (
-        "docker run --rm -it -p 8080:8080 -p 8081:8081 -p 8082:8082 -p 7070:7070 -p 7071:7071 " +
-        f"-v {args.model_store_path}:/home/model-server/model-store " +
-        f"-v {str(base_path/'config.properties')}:/home/model-server/config.properties " +
-        "pytorch/torchserve torchserve --model-store model-store --start --models all --ncs "
-    )
-    print(cmd)
-
-    subprocess.run(cmd.split(' '))
+    #run_with_subprocess(args.model_store_path)
+    run_with_docker_py(args.model_store_path)
