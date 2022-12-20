7f306d08ce615e5503774687b86972e11e7c2c19
davidleonfdez <dleoferu@gmail.com>
2022-12-20 02:21:32 +0000

    Pin TorchServe version to ensure it works with ONNX

--- server/objdetserver/scripts/run_local_docker.py
+++ server/objdetserver/scripts/run_local_docker.py
@@ -4,6 +4,8 @@ import subprocess
 
 
 PORTS = [8080, 8081, 8082, 7070, 7071]
+# 0.7.0 doesn't work with ONNX because of a bug
+TS_IMG_VERSION = "0.6.1-cpu" #"latest-cpu"
 
 
 def _get_base_path():
@@ -18,7 +20,7 @@ def run_with_subprocess(model_store_path):
         f"docker run --rm -it {ports_binding} " +
         f"-v {model_store_path}:/home/model-server/model-store " +
         f"-v {str(base_path/'config.properties')}:/home/model-server/config.properties " +
-        "pytorch/torchserve:latest-cpu torchserve --model-store model-store --start --models all --ncs "
+        f"pytorch/torchserve:{TS_IMG_VERSION} torchserve --model-store model-store --start --models all --ncs "
     )
     print(cmd)
     subprocess.run(cmd.split(' '))
@@ -30,7 +32,7 @@ def run_with_docker_py(model_store_path):
     base_path = _get_base_path()
 
     container = client.containers.run(
-        "pytorch/torchserve:latest-cpu",
+        f"pytorch/torchserve:{TS_IMG_VERSION}",
         command="torchserve --model-store model-store --start --models all --ncs",
         volumes=[
             f"{model_store_path}:/home/model-server/model-store",
